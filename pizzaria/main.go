7cdf4fe65f06155ad906e393cc23197af25ad4f8
Daniel Araújo <daniels_araujo@outlook.com.br>
2024-10-15 19:12:14 -0300

    Criando pacotes

--- pizzaria/main.go
+++ pizzaria/main.go
@@ -1,24 +1,19 @@
 package main
 
 import (
+	"pizzaria/models"
+
 	"github.com/gin-gonic/gin"
 )
-
-type Pizza struct {
-	ID    int     `json:"id"`
-	Nome  string  `json:"nome"`
-	Preco float64 `json:"preco"`
-}
 
 func main() {
 	router := gin.Default()
 	router.GET("/pizzas", getPizzas)
-
 	router.Run()
 }
 
 func getPizzas(c *gin.Context) {
-	var pizzas = []Pizza{
+	var pizzas = []models.Pizza{
 		{ID: 1, Nome: "Toscana", Preco: 49.5},
 		{ID: 2, Nome: "Marguerita", Preco: 79.5},
 		{ID: 3, Nome: "Atum com queijo", Preco: 69.5},
